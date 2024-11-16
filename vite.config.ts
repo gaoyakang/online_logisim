60df1d7f35a242172b0ac5aad7f32c4a9d5d9753
lonely <3060900582@qq.com>
2024-11-16 10:05:01 +0800

    chore: 解决打包时环境选择问题

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { analyzer } from "vite-bundle-analyzer";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    analyzer()
-  ],
-  build: {
-    sourcemap: true // 启用 source map
+function getModeFromArgs() {
+  const args = process.argv;
+  let mode = 'development'; // 默认模式为 dev
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === '--mode') {
+      mode = args[i+1];
+      break;
+    }
   }
+  return mode;
+}
+
+// https://vite.dev/config/ 
+/* @ts-ignore */
+export default defineConfig(({ command }) => {
+  const mode = getModeFromArgs(); // 获取 mode
+  const isProduction = mode === 'production';
+
+  let plugins = [
+    vue(),
+  ];
+
+  if (mode === 'development') {
+    plugins.push(analyzer());
+  }
+  return {
+    plugins,
+    build: {
+      sourcemap: !isProduction, // 启用 source map
+    },
+  };
 })
